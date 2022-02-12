f7bd32674a9fd6ed7be8ce02d7635f2ab88f2e18
程宬典 <ccd284@outlook.com>
2022-02-12 19:05:01 +0800

    Initial commit

---
+++ src/FastScanner.java
@@ -0,0 +1,79 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class FastScanner {
+    public BufferedReader br;
+    public StringTokenizer st;
+
+    public FastScanner(InputStream in) {
+        br = new BufferedReader(new InputStreamReader(in));
+    }
+
+    public FastScanner(String fileName) {
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public int nextInt() {
+        return Integer.parseInt(next());
+    }
+
+    public boolean hasMoreTokens() {
+        while (st == null || !st.hasMoreElements()) {
+            String line = null;
+            try {
+                line = br.readLine();
+            } catch (IOException e) {
+                return false;
+            }
+            if (line == null) {
+                return false;
+            }
+            st = new StringTokenizer(line);
+        }
+        return true;
+    }
+
+    public String next() {
+        while (st == null || !st.hasMoreElements()) {
+            String line = null;
+            try {
+                line = br.readLine();
+            } catch (IOException e) {
+                throw new UnknownError();
+            }
+            if (line == null) {
+                throw new UnknownError();
+            }
+            st = new StringTokenizer(line);
+        }
+        return st.nextToken();
+    }
+
+    public long nextLong() {
+        return Long.parseLong(next());
+    }
+
+    public double nextDouble() {
+        return Double.parseDouble(next());
+    }
+
+    public int[] nextIntArray(int n) {
+        int[] ret = new int[n];
+        for (int i = 0; i < n; i++) {
+            ret[i] = nextInt();
+        }
+        return ret;
+    }
+
+    public long[] nextLongArray(int n) {
+        long[] ret = new long[n];
+        for (int i = 0; i < n; i++) {
+            ret[i] = nextLong();
+        }
+        return ret;
+    }
+}
---
+++ src/SmallChangeSystem.java
@@ -0,0 +1,12 @@
+import java.io.PrintWriter;
+
+public class SmallChangeSystem {
+    public static void main(String[] args) {
+        FastScanner in=new FastScanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int a=in.nextInt();
+        out.println(a);
+        out.close();
+    }
+}
+
