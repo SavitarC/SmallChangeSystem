9a15dce8f5be34cb0e7c62b18abfa69cade7a30b
程宬典 <ccd284@outlook.com>
2022-02-14 12:07:02 +0800

    Initial commit

--- src/SmallChangeSystem.java
+++ src/SmallChangeSystem.java
@@ -1,7 +1,9 @@
 import java.io.*;
 import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 public class SmallChangeSystem {
     public PrintWriter out;
@@ -19,14 +21,32 @@ public class SmallChangeSystem {
             File detail = new File("detail.txt");
             detail.createNewFile();
             BigDecimal total = BigDecimal.valueOf(0);
+            RandomAccessFile raf = new RandomAccessFile("detail.txt", "r");
+            long len = raf.length();
+            String lastLine = "";
+            if (len != 0L) {
+                long pos = len - 1;
+                while (pos > 0) {
+                    pos--;
+                    raf.seek(pos);
+                    if (raf.readByte() == '\n') {
+                        lastLine = raf.readLine();
+                        lastLine = new String(lastLine.getBytes("8859_1"), StandardCharsets.UTF_8);
+                        break;
+                    }
+                }
+                total = new BigDecimal(lastLine.substring(lastLine.lastIndexOf("余额") + 3));
+            }
+            raf.close();
             do {
                 out.println("----------零钱通菜单----------");
                 out.println("        1.零钱通明细");
                 out.println("        2.收益入账");
                 out.println("        3.消费");
-                out.println("        4.退出");
+                out.println("        4.清空记录");
+                out.println("        5.退出");
 
-                out.print("请选择(1-4):");
+                out.print("请选择(1-5):");
                 out.flush();
                 choice = in.next();
                 switch (choice) {
@@ -37,18 +57,43 @@ public class SmallChangeSystem {
                         out.println();
                     }
                     case "2" -> {
-                        out.println("2.收益入账");
+                        System.out.println("2.收益入账");
                         BigDecimal a = BigDecimal.valueOf(in.nextDouble());
-                        total = total.add(a);
-                        income(a, total);
-                        out.println("入账记录成功");
+                        int r=a.compareTo(BigDecimal.ZERO);
+                        if (r < 0){
+                            out.println("数据有误");
+                        }else{
+                            total = total.add(a);
+                            income(a, total);
+                            out.println("入账记录成功");
+                        }
                     }
                     case "3" -> {
-                        out.println("3.消费");
-
+                        System.out.println("3.消费");
+                        BigDecimal a = BigDecimal.valueOf(in.nextDouble());
+                        int r=a.compareTo(BigDecimal.ZERO);
+                        if (r < 0){
+                            out.println("数据有误");
+                        }else{
+                            total = total.subtract(a);
+                            outcome(a, total);
+                            out.println("消费记录成功");
+                        }
                     }
                     case "4" -> {
-                        out.println("4.退出");
+                        System.out.println("4.清空记录");
+                        System.out.println("确认清空记录请输入 y");
+                        String ensure=in.next();
+                        if (Objects.equals(ensure, "y")){
+                            clean(detail);
+                            out.println("已清空");
+                        }else{
+                            out.println("错误，已退出");
+                            loop = false;
+                        }
+                    }
+                    case "5" -> {
+                        out.println("5.退出");
                         loop = false;
                     }
                     default -> out.println("菜单选择有误");
@@ -76,7 +121,7 @@ public class SmallChangeSystem {
         }
     }
 
-    public void income(BigDecimal a, BigDecimal total) throws IOException {
+    public void income(BigDecimal a, BigDecimal total) {
         Date date = new Date();
         SimpleDateFormat time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         FileWriter fw = null;
@@ -96,6 +141,42 @@ public class SmallChangeSystem {
             pw.close();
             fw.close();
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void outcome(BigDecimal a, BigDecimal total) {
+        FastScanner in = new FastScanner(System.in);
+        Date date = new Date();
+        SimpleDateFormat time = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        FileWriter fw = null;
+        try {
+            //如果文件存在，则追加内容；如果文件不存在，则创建文件
+            File f = new File("detail.txt");
+            fw = new FileWriter(f, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        assert fw != null;
+        PrintWriter pw = new PrintWriter(fw);
+        pw.println(in.next() + " -" + a + "  " + time.format(date) + "  余额:" + total);
+        pw.flush();
+        try {
+            fw.flush();
+            pw.close();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void clean(File detail){
+        try {
+            FileWriter fw =new FileWriter(detail);
+            fw.write("\n");
+            fw.flush();
+            fw.close();
+        }catch (IOException e){
             e.printStackTrace();
         }
     }
